1406fe7985619069eb6d4af0708bc2350f0a2dc6
Yavuz Eroğlu <106495712+yavuzeroglu@users.noreply.github.com>
2022-06-25 19:25:43 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace hackerrank_staircase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Basamak Sayısını Giriniz");
+            int step = int.Parse(Console.ReadLine());
+             for(int i=0; i<step;i++) {
+                for(int x= step-1; x>i;x--)
+                {
+                    Console.Write(" ");
+                }
+                for(int y=0;y <=i;y++) {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+             }
+        }
+    }
+}
